2a6c38d09a7d97b048dbde293daccb195678ed8d
Dat <kaistdat123@gmail.com>
2021-02-19 22:08:46 +0700

    only one thing left

--- goodbye_kurama.c
+++ goodbye_kurama.c
@@ -6,4 +6,5 @@ int main()
     printf("There are still a lot of things i want to tell you, but ... \n");
     printf("Let's come back once day \n");
     printf("Added only this file to the end \n");
+    printf("only one thing left \n");
 }
